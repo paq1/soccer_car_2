0005fbcb91ae8cd193a68d119129ee8155498ea6
paq1 <p.paquin1016@laposte.net>
2018-07-18 14:38:58 +0200

    paq1 : mise en place de la sfml

--- soccer_2d/soccer_2d/main.cpp
+++ soccer_2d/soccer_2d/main.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
 #include <string>
+#include <SFML/Graphics.hpp>
 
 using namespace std;
 
 int main(int argc, char ** argv) {
 	// debut du programme
 	cout << "program *************************** debut" << endl;
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
 	cout << "program *************************** fin  " << endl;
 	getchar();
 	return EXIT_SUCCESS;
