e9626340021f4bd000b04e56fc4a7f2e58bbe057
paq1 <p.paquin1016@laposte.net>
2018-07-19 14:37:42 +0200

    paq1 : amelioration du stade

--- soccer_2d/soccer_2d/stade.cpp
+++ soccer_2d/soccer_2d/stade.cpp
@@ -1,14 +1,44 @@
 #include "stade.hpp"
 
 Stade::Stade() {
-	this->texture.loadFromFile("assets/images/stade/stade_1.png");
-	this->sprite.setTexture(this->texture);
+	// init position
+	this->pos_imiko = sf::Vector2f(300, -5);
+
+	this->droite = true;
+	this->gauche = false;
+
+	this->texture_stade.loadFromFile("assets/images/stade/stade_1.png");
+	this->sprite_stade.setTexture(this->texture_stade);
+
+	this->texture_affichage_imigame.loadFromFile(
+		"assets/images/ecran_stade/soccer_affichage_imigame.png"
+	);
+	this->sprite_affichage_imigame.setTexture(
+		this->texture_affichage_imigame
+	);
+	this->sprite_affichage_imigame.setPosition(this->pos_imiko);
+
+	this->texture_ecran1.loadFromFile(
+		"assets/images/ecran_stade/ecran_affichage_2.png"
+	);
+	this->sprite_ecran1.setTexture(
+		this->texture_ecran1
+	);
+	this->sprite_ecran1.setPosition(sf::Vector2f(300, 0));
 }
 
 Stade::~Stade() {
 
 }
 
+void Stade::update() {
+	// if(this->droite)
+
+	this->sprite_affichage_imigame.setPosition(this->pos_imiko);
+}
+
 void Stade::draw(sf::RenderWindow* p_window) {
-	p_window->draw(this->sprite);
+	p_window->draw(this->sprite_stade);
+	p_window->draw(this->sprite_ecran1);
+	p_window->draw(this->sprite_affichage_imigame);
 }
--- soccer_2d/soccer_2d/stade.hpp
+++ soccer_2d/soccer_2d/stade.hpp
@@ -5,12 +5,22 @@
 
 class Stade {
 private:
-	sf::Texture texture;
-	sf::Sprite sprite;
+	sf::Texture texture_stade;
+	sf::Sprite sprite_stade;
+
+	sf::Texture texture_ecran1;
+	sf::Sprite sprite_ecran1;
+
+	sf::Texture texture_affichage_imigame;
+	sf::Sprite sprite_affichage_imigame;
+
+	sf::Vector2f pos_imiko;
+	bool gauche, droite;
 public:
 	Stade();
 	~Stade();
 
+	void update();
 	void draw(sf::RenderWindow*);
 };
 
